5106b495933872276f557b3ce8bf69ede67a96dd
Ilja-Prihach <piratehnik9508@gmail.com>
2025-09-21 22:32:15 +0300

    add Redux thunk in project

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,21 +1,16 @@
 import s from './DecksList.module.css'
 import { useEffect } from 'react'
-import { decksApi } from '../decks-api.ts'
-import { useDispatch } from 'react-redux'
-import { setDecksAC } from '../decks-reducer.ts'
-import { useAppSelector } from '../../../app/store.ts'
+//import { decksApi } from '../decks-api.ts'
+//import { useDispatch } from 'react-redux'
+//import { setDecksAC } from '../decks-reducer.ts'
+//import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
-import { selectDecks } from '../decks-selectors.ts'
+//import { selectDecks } from '../decks-selectors.ts'
+//import { fetchDecksTC } from '../decks-thunks.ts'
+import { useFetchDecks } from './useFetchDecks.ts'
 
 export const DecksList = () => {
-  const dispatch = useDispatch()
-  const decks = useAppSelector(selectDecks)
-
-  useEffect(() => {
-    decksApi.fetchDecks().then((res) => {
-      dispatch(setDecksAC(res.data.items))
-    })
-  }, [])
+  const {decks} = useFetchDecks()
   return <ul className={s.list}>
     {decks.map((deck) => (<DeckItem key={deck.id} deck={deck} />))}
   </ul>

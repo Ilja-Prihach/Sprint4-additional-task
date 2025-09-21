f2661923b9d4fc7d4007c187f59aab285043e32a
Ilja-Prihach <piratehnik9508@gmail.com>
2025-09-21 21:53:41 +0300

    start repo, and writing get request

---
+++ src/features/decks/decks-api.ts
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+export const instance = axios.create({
+  baseURL: 'https://api.flashcards.andrii.es',
+  headers: {
+    'x-auth-skip': true,
+  },
+})
+
+export const decksApi = {
+  fetchDecks() {
+    return instance.get(`v2/decks`)
+  },
+}
+
+export type FetchDeckResponse = {
+  items: Deck[]
+  pagination: Pagination
+  maxCardsCount: number
+}
+export type Author = {
+  id: string
+  name: string
+}
+
+export type Deck = {
+  author: Author
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: string
+  updated: string
+  cardsCount: number
+}
+export type Pagination = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
